d8bb92abd443cdb50f760a1aa8dca7a9c3c995d3
marcosmesquita1980 <mesquita.juanmarcos@gmail.com>
2018-12-17 19:02:29 +0100

    hasta capitulo 025

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('categoria', require('./components/Categoria.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -31,6 +31,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
     data :{
-        menu :0
+        menu : 0
     }
 });
